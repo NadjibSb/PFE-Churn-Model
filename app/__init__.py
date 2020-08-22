ca27f22a17bccbc04f57199bd91c80a495c99dde
NadjibSb <fn_souab@esi.dz>
2020-08-23 00:38:42 +0100

    Knowledge component

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
 import time
-from app.mapek.monitor import Monitor
 # Suppressing Warnings
 import warnings
 warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
-from app import views
+#from app import views
 
+
+from app.mapek.monitor import Monitor
+from app.mapek.knowledge import Knowledge
+
+
+k = Knowledge.getInstance()
+k.save("test",3333333)
+print(k.get("test"))
 
 '''
 monitor = Monitor()
---
+++ app/mapek/knowledge.py
@@ -0,0 +1,45 @@
+import time
+import json
+
+class Knowledge:
+    __instance = None
+    __dict = {}
+    
+    @staticmethod 
+    def getInstance():
+        if Knowledge.__instance == None:
+            Knowledge()
+            print("Loaded JSON:")
+            print(Knowledge.__dict)
+        return Knowledge.__instance
+        
+    def __init__(self):
+        if Knowledge.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            Knowledge.__instance = self
+            Knowledge.__dict = self.loadJson()
+
+
+    def saveJson(self):
+        with open('knowledge.json', 'w') as fp:
+            json.dump(Knowledge.__dict, fp,indent=4)
+
+
+    def loadJson(self):
+        with open('knowledge.json', 'r') as fp:
+            data = json.load(fp)
+            return data
+
+    
+    def save(self,key,value):
+        if key not in Knowledge.__dict:
+            Knowledge.__dict[key]= {}
+        Knowledge.__dict[key]["value"] = value
+        if "history" not in Knowledge.__dict[key]:
+            Knowledge.__dict[key]["history"] = []
+        Knowledge.__dict[key]["history"].append({"date": time.time(), "value": value}) 
+        self.saveJson()
+    
+    def get(self,key):
+        return Knowledge.__dict[key]
